4a12a66a779618fb13772a78646fb28f9d4b02eb
FelipeKutschera <165940217+FelipeKutschera@users.noreply.github.com>
2024-11-14 10:29:40 -0300

    Update Usuario.cs

--- mf-dev-backend-2023/Models/Usuario.cs
+++ mf-dev-backend-2023/Models/Usuario.cs
@@ -1,4 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace mf_dev_backend_2023.Models
@@ -9,20 +9,24 @@ namespace mf_dev_backend_2023.Models
         [Key]
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "Obrigatoio informar o nome")]
+        [Required(ErrorMessage = "Obrigatório informar o nome")]
         public string Nome { get; set; }
 
-        [Required(ErrorMessage = "Obrigatoio informar a senha")]
+        [Required(ErrorMessage = "Obrigatório informar a senha")]
         [DataType(DataType.Password)]
         public string Senha { get; set; }
 
-        [Required(ErrorMessage = "Obrigatoio informar a senha")]
+        [Required(ErrorMessage = "Obrigatório informar o e-mail")]
+        [EmailAddress(ErrorMessage = "Formato de e-mail inválido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o perfil")]
         public Perfil Perfil { get; set; }
+
         public string? TokenRedefinicaoSenha { get; set; }
         public DateTime? TokenValidade { get; set; }
-
-
     }
+
     public enum Perfil
     {
         Admin,
